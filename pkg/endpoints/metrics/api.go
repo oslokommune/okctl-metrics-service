d84dc2f6b0c7665a6fa975a356d27104acb87732
Julius Pedersen <deifyed@tutanota.com>
2021-10-21 09:53:48 +0200

    👌 Improve labels usage

--- pkg/endpoints/metrics/api.go
+++ pkg/endpoints/metrics/api.go
@@ -1,7 +1,10 @@
 package metrics
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"net/http"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 
@@ -20,4 +23,24 @@ func GetRoutes(cfg config.Config, logger *logrus.Logger) endpoints.Routes {
 			HandlerFunc: generateMetricHandler(cfg, logger),
 		},
 	}
+}
+
+// NewEvent initializes a metric event
+func NewEvent() Event {
+	return Event{Labels: make(map[string]string)}
+}
+
+// Hash generates a consequent string representing an event
+func (receiver Event) Hash() string {
+	hasher := sha256.New()
+
+	parts := []string{
+		receiver.Category.String(),
+		receiver.Action.String(),
+		mapAsString(receiver.Labels),
+	}
+
+	_, _ = hasher.Write([]byte(strings.Join(parts, "")))
+
+	return base64.URLEncoding.EncodeToString(hasher.Sum(nil))
 }
