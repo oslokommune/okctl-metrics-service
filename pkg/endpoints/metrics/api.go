9105a64aacfee8b0542ced6badaa58fb90a3a465
Julius Pedersen <deifyed@tutanota.com>
2021-11-01 12:02:16 +0100

    👌 Remove redundant hash function

--- pkg/endpoints/metrics/api.go
+++ pkg/endpoints/metrics/api.go
@@ -1,10 +1,7 @@
 package metrics
 
 import (
-	"crypto/sha256"
-	"encoding/base64"
 	"net/http"
-	"strings"
 
 	"github.com/sirupsen/logrus"
 
@@ -34,19 +31,4 @@ func GetRoutes(cfg config.Config, logger *logrus.Logger) (endpoints.Routes, []en
 // NewEvent initializes a metric event
 func NewEvent() Event {
 	return Event{Labels: make(map[string]string)}
-}
-
-// Hash generates a consequent string representing an event
-func (receiver Event) Hash() string {
-	hasher := sha256.New()
-
-	parts := []string{
-		receiver.Category.String(),
-		receiver.Action.String(),
-		mapAsString(receiver.Labels),
-	}
-
-	_, _ = hasher.Write([]byte(strings.Join(parts, "")))
-
-	return base64.URLEncoding.EncodeToString(hasher.Sum(nil))
 }
